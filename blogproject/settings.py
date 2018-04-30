b60d9df71cd450feaa978e3ae11209e52358b059
triumph1001 <1455337455@qq.com>
2018-04-30 22:30:40 +0800

    second

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'a8%8vqvrt1t)^z6!5f3re=4pcg!#&c8(0^$o4fh#au5i1h@wh&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.triumphw.cn']
 
 
 # Application definition
@@ -120,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
